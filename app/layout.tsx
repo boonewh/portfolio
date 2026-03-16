a1bd0ad40a90cdc9dda25fd221dde908971336d7
boonewh <boonewh@gmail.com>
2026-03-15 22:12:05 -0500

    fixing problem where Next.js default light and dark modes were screwed up. Dark only now

--- app/layout.tsx
+++ app/layout.tsx
@@ -84,7 +84,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <head>
         <link
           rel="stylesheet"
